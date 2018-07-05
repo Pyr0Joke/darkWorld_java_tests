d345ccb1627259631f943d5fb2b226a2bcc0b2a3
Pyr0Joke <shmel9311@gmail.com>
2018-07-06 00:35:22 +0300

    + loginDarkChrome

--- src/main/java/darkConfig.java
+++ src/main/java/darkConfig.java
@@ -13,15 +13,24 @@ public class darkConfig {
     public String announce = "http://dark-world.ru/announce/";
     public String findGroup = "http://dark-world.ru/search/?q=";
 
-    public WebDriver loginDark(WebDriver driver){
-//        System.setProperty("webdriver.chrome.driver", "D:\\Gecko\\chromedriver.exe");
+    public WebDriver loginDarkFireFox(WebDriver driver){
         System.setProperty("webdriver.gecko.driver", "D:\\Gecko\\geckodriver.exe");
         FirefoxBinary firefoxBinary = new FirefoxBinary();
         firefoxBinary.addCommandLineOptions("--headless");
         FirefoxOptions firefoxOptions = new FirefoxOptions();
         firefoxOptions.setBinary(firefoxBinary);
         driver = new FirefoxDriver(firefoxOptions);
-//        driver = new ChromeDriver();
+        driver.get("http://dark-world.ru/main/");
+        driver.findElement(By.id("Login")).sendKeys("shmel9311@gmail.com");
+        driver.findElement(By.id("Password")).sendKeys("458854qwe");
+        driver.findElement(By.id("loginBtn")).click();
+        WebElement dynamicElement = (new WebDriverWait(driver, 20)).until(ExpectedConditions.presenceOfElementLocated(By.id("logoffLink")));
+        return driver;
+    }
+
+    public WebDriver loginDarkChrome(WebDriver driver){
+        System.setProperty("webdriver.chrome.driver", "D:\\Gecko\\chromedriver.exe");
+        driver = new ChromeDriver();
         driver.get("http://dark-world.ru/main/");
         driver.findElement(By.id("Login")).sendKeys("shmel9311@gmail.com");
         driver.findElement(By.id("Password")).sendKeys("458854qwe");
